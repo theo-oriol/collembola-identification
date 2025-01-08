6fd89657d32b23c97b2556a97864eed97bc9fc32
theo-oriol <theo.oriol@lilo.org>
2025-01-09 00:24:01 +0100

    routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { SigninComponent } from './pages/signin/signin.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: AppComponent },
+    { path: 'signin', component: SigninComponent },
+];
+
