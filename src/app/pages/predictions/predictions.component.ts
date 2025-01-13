c0c02c3d9945a1d5e739395174b564fd6a335c00
theo-oriol <theo.oriol@lilo.org>
2025-01-13 20:40:23 +0100

    predictions page

---
+++ src/app/pages/predictions/predictions.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-predictions',
+  imports: [],
+  templateUrl: './predictions.component.html',
+  styleUrl: './predictions.component.css'
+})
+export class PredictionsComponent {
+
+}
